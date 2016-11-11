1fdc37ab5e96de7a4ef96c2d67b3b5cf835b40d0
Enrico Salvucci <enrico.salvucci@studio.unibo.it>
2016-11-11 18:06:27 +0000

    Primo commit

---
+++ vignolaBot.py
@@ -0,0 +1,31 @@
+import telepot
+import sys
+import time 
+import random
+import datetime
+import vignolaBotToken
+ 
+# funzione che viene eseguita ad ogni messaggio ricevuto
+def handle(message):
+	chat_id = message['chat']['id']
+	command = message['text']
+	import random
+
+	foo = ['E\' bugggggia', 'uagliooo!', 'stasera devo programmare...', 'st\'Arduino!', 'Uaglio so arrivat ', 'eeeeeeeee credo di si...no buggia, \'sto weekend sto con Azzurra']
+	print(random.choice(foo))	
+
+	print 'Ho ricevuto il comando %s' % command
+
+#			TODO			#
+#	if command == '/help':
+# 		bot.sendMessage(chat_id,messaggio)
+	if command == '/random':
+ 		bot.sendMessage(chat_id, random.choice(foo))
+
+bot = telepot.Bot(vignolaBotToken.token)
+bot.message_loop(handle)
+
+print 'In attesa di nuovi messaggi...'
+
+while 1:
+	time.sleep(100)
