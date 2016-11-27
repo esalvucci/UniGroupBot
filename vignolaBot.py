bf4c38fa9e9c8812ab3afca13ace34e6e6f361a7
RyuzakiKK <aasonykk@gmail.com>
2016-11-27 21:12:11 +0100

    Fixed following PEP 8, and indented the print of debug

--- vignolaBot.py
+++ vignolaBot.py
@@ -1,34 +1,37 @@
 import telepot
 import sys
-import time 
+import time
 import random
 import datetime
 import vignolaBotToken
- 
+
+
 # funzione che viene eseguita ad ogni messaggio ricevuto
 def handle(message):
-	chat_id = message['chat']['id']
-	command = message['text']
-	import random
+    chat_id = message['chat']['id']
+    command = message['text']
+    import random
 
-	foo = ['E\' bugggggia', 'uagliooo!', 'stasera devo programmare...', 'st\'Arduino!', 'Uaglio so arrivat ', 'eeeeeeeee credo di si...no buggia, \'sto weekend sto con Azzurra', 'Tonno e cipodda per fa lu soffrittu..capi\'?']
-	
-print(random.choice(foo))	
+    foo = ['E\' bugggggia', 'uagliooo!', 'stasera devo programmare...', 'st\'Arduino!', 'Uaglio so arrivat ',
+           'eeeeeeeee credo di si...no buggia, \'sto weekend sto con Azzurra',
+           'Tonno e cipodda per fa lu soffrittu..capi\'?']
 
-	print 'Ho ricevuto il comando %s' % command
+    print(random.choice(foo))
 
-#			TODO			#
-#	if command == '/help':
-# 		bot.sendMessage(chat_id,messaggio)
-	if command == '/start':
- 		bot.sendMessage(chat_id, 'Esegui il comando /random per richiedere una frase di Vignola a caso')
-	elif command == '/random':
- 		bot.sendMessage(chat_id, random.choice(foo))
+    print('Ho ricevuto il comando %s' % command)
+
+    # TODO
+    # if command == '/help':
+    # bot.sendMessage(chat_id, messaggio)
+    if command == '/start':
+        bot.sendMessage(chat_id, 'Esegui il comando /random per richiedere una frase di Vignola a caso')
+    elif command == '/random':
+        bot.sendMessage(chat_id, random.choice(foo))
 
 bot = telepot.Bot(vignolaBotToken.token)
 bot.message_loop(handle)
 
-print 'In attesa di nuovi messaggi...'
+print('In attesa di nuovi messaggi...')
 
 while 1:
-	time.sleep(100)
+    time.sleep(100)
