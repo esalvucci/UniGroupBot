d9885bd9980976a8d8ed6c9d44a33fc7186f0e02
Enrico Salvucci <enrico.salvucci@studio.unibo.it>
2016-11-29 18:38:53 +0000

    .

--- vignolaBot.py
+++ vignolaBot.py
@@ -9,11 +9,10 @@ import vignolaBotToken
 def handle(message):
 	chat_id = message['chat']['id']
 	command = message['text']
-	import random
 
 	foo = ['E\' bugggggia', 'uagliooo!', 'stasera devo programmare...', 'st\'Arduino!', 'Uaglio so arrivat ', 'eeeeeeeee credo di si...no buggia, \'sto weekend sto con Azzurra', 'Tonno e cipodda per fa lu soffrittu..capi\'?']
 	
-print(random.choice(foo))	
+	print(random.choice(foo))	
 
 	print 'Ho ricevuto il comando %s' % command
 
@@ -21,9 +20,9 @@ print(random.choice(foo))
 #	if command == '/help':
 # 		bot.sendMessage(chat_id,messaggio)
 	if command == '/start':
- 		bot.sendMessage(chat_id, 'Esegui il comando /random per richiedere una frase di Vignola a caso')
+		bot.sendMessage(chat_id, 'Esegui il comando /random per richiedere una frase di Vignola a caso')
 	elif command == '/random':
- 		bot.sendMessage(chat_id, random.choice(foo))
+		bot.sendMessage(chat_id, random.choice(foo))
 
 bot = telepot.Bot(vignolaBotToken.token)
 bot.message_loop(handle)
